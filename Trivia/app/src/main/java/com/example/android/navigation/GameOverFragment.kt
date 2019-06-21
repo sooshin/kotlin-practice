12ed0b83d8cd6c84907b6abe8545b8f986a47835
Soojeong <soojeongshin2@gmail.com>
2019-06-21 14:39:37 +0900

    feat: 3.07-Adding Safe Arguments

--- Trivia/app/src/main/java/com/example/android/navigation/GameOverFragment.kt
+++ Trivia/app/src/main/java/com/example/android/navigation/GameOverFragment.kt
@@ -32,7 +32,8 @@ class GameOverFragment : Fragment() {
         val binding: FragmentGameOverBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_over, container, false)
         binding.tryAgainButton.setOnClickListener { view: View ->
-            view.findNavController().navigate(R.id.action_gameOverFragment_to_gameFragment)
+//            view.findNavController().navigate(R.id.action_gameOverFragment_to_gameFragment)
+            view.findNavController().navigate(GameOverFragmentDirections.actionGameOverFragmentToGameFragment())
         }
         return binding.root
     }
--- Trivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ Trivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.FragmentTitleBinding
@@ -20,9 +19,12 @@ class TitleFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_title, container, false)
-        binding.playButton.setOnClickListener(
-                Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
-        )
+//        binding.playButton.setOnClickListener(
+//                Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+//        )
+        binding.playButton.setOnClickListener { v: View ->
+            v.findNavController().navigate(TitleFragmentDirections.actionTitleFragmentToGameFragment())
+        }
         // This tells Android that our fragment has an Options Menu, so it will call
         // onCreateOptionsMenu
         setHasOptionsMenu(true)
