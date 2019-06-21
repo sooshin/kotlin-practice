694987f1355cb3c3bb8d26b295cd0ba7bd8e52cc
Soojeong <soojeongshin2@gmail.com>
2019-06-21 16:09:04 +0900

    feat: 3.09-Adding the Navigation Drawer

--- Trivia/app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ Trivia/app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -19,24 +19,35 @@ package com.example.android.navigation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        drawerLayout = binding.drawerLayout
+
         // Find the navController from myNavHostFragment
         val navController = this.findNavController(R.id.myNavHostFragment)
         // Link the navController to our ActionBar
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         // Find the navController and then call navController.navigateUp
         val navController = this.findNavController(R.id.myNavHostFragment)
-        return navController.navigateUp()
+//        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
