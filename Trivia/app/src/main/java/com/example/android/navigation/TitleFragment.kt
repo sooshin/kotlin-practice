600eac4f377327465112ba5234963b0906128e0c
Soojeong <soojeongshin2@gmail.com>
2019-06-21 11:58:52 +0900

    feat: 3.06-Adding the About Menu

--- Trivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ Trivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -2,12 +2,12 @@ package com.example.android.navigation
 
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 /**
@@ -23,6 +23,23 @@ class TitleFragment : Fragment() {
         binding.playButton.setOnClickListener(
                 Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
         )
+        // This tells Android that our fragment has an Options Menu, so it will call
+        // onCreateOptionsMenu
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+        // Inflate menu resource
+        inflater?.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        // Return true if NavigationUI.onNavDestinationSelected returns true, else return
+        // super.onOptionsItemSelected.
+        return NavigationUI.onNavDestinationSelected(item!!,
+                view!!.findNavController())
+        || super.onOptionsItemSelected(item)
     }
 }
