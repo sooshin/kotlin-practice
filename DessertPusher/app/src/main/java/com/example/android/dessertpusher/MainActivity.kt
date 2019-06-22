48563992aafc7a0980df538fea74df4d657e0367
Soojeong <soojeongshin2@gmail.com>
2019-06-23 00:24:32 +0900

    feat: 4.04-Add the lifecycle library

--- DessertPusher/app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
+++ DessertPusher/app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
@@ -76,7 +76,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
         }
 
         // Setup dessertTimer
-        dessertTimer = DessertTimer()
+        dessertTimer = DessertTimer(this.lifecycle)
 
         // Set the TextViews to the right values
         binding.revenue = revenue
@@ -155,7 +155,6 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
 
     override fun onStart() {
         super.onStart()
-        dessertTimer.startTimer()
         Timber.i("onStart Called")
     }
 
@@ -171,7 +170,6 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
 
     override fun onStop() {
         super.onStop()
-        dessertTimer.stopTimer()
         Timber.i("onStop Called")
     }
 
