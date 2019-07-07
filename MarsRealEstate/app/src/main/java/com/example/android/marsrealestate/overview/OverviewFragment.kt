dd61b9e72a1abe1b616f789359c4331d2d180547
Soojeong <soojeongshin2@gmail.com>
2019-07-07 23:25:06 +0900

    feat: 8.05-Displaying a Grid of Internet Images

--- MarsRealEstate/app/src/main/java/com/example/android/marsrealestate/overview/OverviewFragment.kt
+++ MarsRealEstate/app/src/main/java/com/example/android/marsrealestate/overview/OverviewFragment.kt
@@ -22,7 +22,7 @@ import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.example.android.marsrealestate.R
-import com.example.android.marsrealestate.databinding.GridViewItemBinding
+import com.example.android.marsrealestate.databinding.FragmentOverviewBinding
 
 /**
  * This fragment shows the the status of the Mars real-estate web services transaction.
@@ -42,13 +42,16 @@ class OverviewFragment : Fragment() {
      */
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val binding = GridViewItemBinding.inflate(inflater)
+        val binding = FragmentOverviewBinding.inflate(inflater)
 
         // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
         binding.setLifecycleOwner(this)
 
         // Giving the binding access to the OverviewViewModel
         binding.viewModel = viewModel
+
+        // Sets the adapter of the photosGrid RecyclerView
+        binding.photosGrid.adapter = PhotoGridAdapter()
 
         setHasOptionsMenu(true)
         return binding.root
