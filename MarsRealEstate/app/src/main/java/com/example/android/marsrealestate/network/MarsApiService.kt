2a64f32316cc9f0c14ed6b81d225db193f4b99a8
Soojeong <soojeongshin2@gmail.com>
2019-07-07 16:06:41 +0900

    feat: 8.01-Making Mars Web Service Contact

--- MarsRealEstate/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ MarsRealEstate/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -17,4 +17,41 @@
 
 package com.example.android.marsrealestate.network
 
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
 private const val BASE_URL = "https://mars.udacity.com/"
+
+/**
+ * Use the Retrofit builder to build a retrofit object using a Moshi converter with our Moshi
+ * object pointing to the desired URL
+ */
+private val retrofit = Retrofit.Builder()
+        .addConverterFactory(ScalarsConverterFactory.create())
+        .baseUrl(BASE_URL)
+        .build()
+
+/**
+ * A public interface that exposes the [getProperties] method
+ */
+interface MarsApiService {
+    /**
+     * Returns a Retrofit callback that delivers a String
+     * The @GET annotation indicates that the "realestate" endpoint will be requested with the GET
+     * HTTP method
+     */
+    @GET("realestate")
+    fun getProperties():
+            Call<String>
+}
+
+/**
+ * A public Api object that exposes the lazy-initialized Retrofit service
+ */
+object MarsApi {
+    val retrofitService : MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java)
+    }
+}
