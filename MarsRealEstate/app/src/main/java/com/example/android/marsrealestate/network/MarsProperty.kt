87b6fa15dd33befddaf7e75885b53c547a9b83b2
Soojeong <soojeongshin2@gmail.com>
2019-07-07 17:23:51 +0900

    feat: 8.02-Parsing JSON With Moshi

--- MarsRealEstate/app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
+++ MarsRealEstate/app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
@@ -17,4 +17,16 @@
 
 package com.example.android.marsrealestate.network
 
-class MarsProperty()
+import com.squareup.moshi.Json
+
+/**
+ * This data class defines a Mars property which includes an ID, the image URL, the type (sale
+ * or rental) and the price (monthly if it's a rental).
+ * The property names of this data class are used by Moshi to match the names of values in JSON.
+ */
+data class MarsProperty(
+        val id: String,
+        @Json(name = "img_src") val imgSrcUrl: String,
+        val type: String,
+        val price: Double
+)
