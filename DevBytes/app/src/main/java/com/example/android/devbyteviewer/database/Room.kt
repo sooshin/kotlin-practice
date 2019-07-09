45fb6979a1d4ce28becf5437500169d1b31ef751
Soojeong <soojeongshin2@gmail.com>
2019-07-09 16:58:57 +0900

    feat: 9.02-Add VideoDao

--- DevBytes/app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
+++ DevBytes/app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
@@ -16,3 +16,17 @@
  */
 
 package com.example.android.devbyteviewer.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface VideoDao {
+    @Query("select * from databasevideo")
+    fun getVideos(): List<DatabaseVideo>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(vararg videos: DatabaseVideo)
+}
