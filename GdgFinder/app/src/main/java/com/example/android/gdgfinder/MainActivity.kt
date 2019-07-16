54b36da543a4ba69a35bf3f2fb4fb3e43cdfd559
Soojeong <soojeongshin2@gmail.com>
2019-07-16 22:20:23 +0900

    feat: 10.13-Add Dark Mode Support

--- GdgFinder/app/src/main/java/com/example/android/gdgfinder/MainActivity.kt
+++ GdgFinder/app/src/main/java/com/example/android/gdgfinder/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.android.gdgfinder
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
@@ -20,6 +21,8 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         setupNavigation()
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
     }
 
     /**
